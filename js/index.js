35fd11172762bd689ad3072996c62c881b40fa8e
Sveta K <test@test.test>
2019-07-28 23:15:09 +0100

    Make the navigation bar responsive

--- js/index.js
+++ js/index.js
@@ -1,4 +1,16 @@
 function navbarToggle() {
-  
-
+    let elem= document.getElementById("navbar-top"); 
+    
+    if (elem.className === "navigation-bar") {
+        elem.className += " responsive";
+      } else {
+        elem.className = "navigation-bar";
+      }
+   let icon = document.getElementById("menu-icon");
+    if(icon.className.includes('fa-bars')) {
+        icon.className = "fa fa-times"
+    }
+    else {
+        icon.className = "fa fa-bars"
+    }
 }
