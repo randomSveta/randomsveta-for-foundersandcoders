386323918faf7fc6f8baff0e067faf538bce43b6
Sveta K <test@test.test>
2019-07-30 17:18:44 +0100

    Add certificates images, implement carousel with min styles and sections frames, change height for blocks

--- js/index.js
+++ js/index.js
@@ -1,16 +1,50 @@
 function navbarToggle() {
-    let elem= document.getElementById("navbar-top"); 
-    
+    let elem = document.getElementById("navbar-top");
+
     if (elem.className === "navigation-bar") {
         elem.className += " responsive";
-      } else {
+    } else {
         elem.className = "navigation-bar";
-      }
-   let icon = document.getElementById("menu-icon");
-    if(icon.className.includes('fa-bars')) {
+    }
+    let icon = document.getElementById("menu-icon");
+    if (icon.className.includes('fa-bars')) {
         icon.className = "fa fa-times"
     }
     else {
         icon.className = "fa fa-bars"
     }
+}
+
+let slideIndex = 1;
+window.onload = function() {
+    showSlides(slideIndex);
+  };
+
+function plusNumberOfSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    var slides = document.getElementsByClassName("slides");
+    var dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) { 
+        slideIndex = slides.length 
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" fill-color", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " fill-color";
 }
